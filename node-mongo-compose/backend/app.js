cf02b3d7c1c19127c35c2ffdf90bc573cdc3dfb6
Gabriela Almeida <almeidagabrielav@gmail.com>
2020-06-19 20:13:14 -0300

    Finalizando o Cadastro

--- node-mongo-compose/backend/app.js
+++ node-mongo-compose/backend/app.js
@@ -2,13 +2,29 @@ const express = require('express')
 const restful = require('node-restful')
 const server = express()
 const mongoose = restful.mongoose
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 //Database
 mongoose.Promisse = global.Promisse //Mongoose utilizar a api de Promisse do node pois a do mongoose ta deprecated
 mongoose.connect('mongodb://db/mydb')
 
-//Teste
-server.get('/', (req, res, next) => res.send('Backend'))
+//Middlewares
+server.use(bodyParser.urlencoded({extended:true}))
+server.use(bodyParser.json())
+server.use(cors())
+
+//ODM
+const Client = restful.model('Client', {
+    name: { type: String, required: true}
+})
+
+//Rest API
+Client.methods(['get', 'post', 'put', 'delete'])
+Client.updateOptions({new: true, runValidators: true})
+
+//Routes
+Client.register(server, '/clients')
 
 //Start Server
 server.listen(3000)
